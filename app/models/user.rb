7de5fc94494b95692f70fbc0459450ee9cf79ade
Aaron Thornton <aaronthornton00@gmail.com>
2015-08-01 15:03:21 +1200

    fix yml

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates_presence_of :email
+  validates_presence_of :first_name, :last_name, :email, :dob
 
   has_many :allocations
   has_many :games, through: :allocations
